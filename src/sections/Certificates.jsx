8e8b1cbbe5f1803c595347a1e3116b50ec93d329
Manas Mayank <mayank.manas.2801@gmail.com>
2026-07-02 09:36:51 +0530

    Updated resume and certifications

--- src/sections/Certificates.jsx
+++ src/sections/Certificates.jsx
@@ -1,30 +1,52 @@
 import "../styles/Certificates.css";
 
 function Certificates() {
+  const certificates = [
+    {
+      title: "TCS iON Career Edge",
+      subtitle: "Young Professional Program",
+      link: "https://drive.google.com/file/d/1JgfAt1xK3V2EoZ0iFOgxwtwJhINYev8a/view?usp=drive_link",
+    },
+    {
+      title: "NPTEL",
+      subtitle: "Data Science Using Python",
+      link: "https://drive.google.com/file/d/1tBUrmPhTLzAhPK8ww2qXHQprYu12-ZSe/view?usp=drive_link",
+    },
+    {
+      title: "Web Development Bootcamp",
+      subtitle: "Udemy Certification",
+      link: "https://drive.google.com/file/d/1YlNN8aOdquMXRZ_YnyvDkUz8REo3i8gk/view?usp=drive_link",
+    },
+    {
+      title: "Web Development with Python",
+      subtitle: "Froyo Technologies",
+      link: "https://drive.google.com/file/d/1J10mIyJoZX7s3-TpYOcLn9pO6r1gCFS5/view?usp=drive_link",
+    },
+    {
+      title: "Microsoft",
+      subtitle: "Introduction to MS Excel",
+      link: "https://drive.google.com/file/d/1N66xpAMAwS7YaOJIKnnUKsD9ruTVWj0N/view?usp=drive_link",
+    },
+  ];
+
   return (
     <section id="certificates" className="certificates">
-
       <h2>Certifications</h2>
 
       <div className="certificate-grid">
-
-        <div className="certificate-card">
-          <h3>TCS iON Career Edge</h3>
-          <p>Young Professional Program</p>
-        </div>
-
-        <div className="certificate-card">
-          <h3>NPTEL</h3>
-          <p>Data Science Using Python</p>
-        </div>
-
-        <div className="certificate-card">
-          <h3>Web Development Bootcamp</h3>
-          <p>Udemy Certification</p>
-        </div>
-
+        {certificates.map((cert, index) => (
+          <a
+            key={index}
+            href={cert.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="certificate-card"
+          >
+            <h3>{cert.title}</h3>
+            <p>{cert.subtitle}</p>
+          </a>
+        ))}
       </div>
-
     </section>
   );
 }
